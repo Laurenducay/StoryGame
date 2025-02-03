cd2420c5bbb4ab9bc13343560cc1d72976a13b06
John Doe <Lauren.Ducay@example.com>
2025-02-03 12:44:01 -0800

    adds more paths and prompts for user.

--- gameData.js
+++ gameData.js
@@ -1,8 +1,25 @@
 let playerName;
 let introText = "today you will be embarking on a journey. While adventuring through the rocky terrain, you encounter a wild Mimikyu! You are left with a choice of whether or not to capture this pokemon.";
-let endText1 = "Without any pokemon in your inventory to defend you, you are defeated and can no longer continue.";
-let endText2 = "The wild Mimikyu remains unbothered... Thankfully you survive your first pokemon encounter. You safely continue your journey through the rocky mountains.";
+let endText1 = "Without any intentions battle, you are defeated and can no longer continue.";
+let endText2 = "The wild Mimikyu remains unbothered... Thankfully you survive your first pokemon encounter. You safely continue your journey alongside the pokemon you choose to adventure with you.";
+let endText3 = "Yay!! You defeated Mimikyu. You can now continue on with your adventure.";
 let pathChoice;
+let pathChoice2;
+let pathChoice3;
+let pathChoice4;
+let pathChoice5;
 let pathText1 = "OH NO!!! The pokemon has revealed their truest form!!";
+let pathTextY = "*Gengar is thrown into battle.*"
+let pathTextN = "Mimikyus' form is only getting stronger...";
+let pathText3;
 let pathText2 = "You have decided to not capture the Mimikyu!";
 let invalid = "Invalid Input.";
+let battle = "...Gengar successfully defeats Mimikyu. You can now capture the pokemon and continue on with your adventure.";
+let paldeaText = "Alright! Let's head to the Paldea region.";
+let sprigatito = "Yay!! Sprigatito has been chosen.";
+let fuecoco = "Yay!! Fuecoco has been chosen.";
+
+let kantoText = "Alright! Let's head to the Kanto Region.";
+let charmander = "Yay!! Charmander has been chosen.";
+let squirtle = "Yay!! Squirtle has been chosen.";
+
--- story.js
+++ story.js
@@ -5,15 +5,63 @@ alert(`Hi there ${playerName}! Welcome.`);
 alert(playerName + ", " + introText);
 pathChoice = prompt("Will you try to capture a wild Mimikyu?");
 
-if(pathChoice == 'yes' || pathChoice == 'Yes') {
+if(pathChoice == "Yes" || pathChoice == "yes") {
     alert(pathText1);
-    alert(endText1);
+    pathChoice2 = prompt("Do you wish to battle?");
+    if(pathChoice2 == "Yes") {
+        alert(pathTextY)
+    }
+    else if(pathChoice2 == "No" || pathChoice2 == "no") {
+        alert(pathTextN)
+        pathText3 = prompt("Are you sure you do not want to battle? (Yes or No)");
+        if(pathText3== "No" || pathText3 == "no") {
+            alert(pathTextY);
+            alert(battle);
+        }
+        else if (pathText3 == "Yes" || pathText3 == "yes") {
+            alert(endText1);
+            alert("--------------GAME OVER--------------");
+        }
+        else {
+            alert(invalid);
+        }
+    }
+    else {
+        alert(invalid);
+    }
 }
-else if(pathChoice == 'no' || pathChoice == 'No') {
+else if(pathChoice == 'no' || pathChoice == "No") {
     alert(pathText2);
-    alert(endText2);
-}
-else {
+    pathChoice3 = prompt("Where would you like to head now? Left: Paldea Region or Right: Kanto Region. Left or Right?");
+    if(pathChoice3 == "Left" || pathChoice3 == "left") {
+        alert(paldeaText);
+        pathChoice4 = prompt("Which Pokemon would you like to take? Enter 1 for Fuecoco or 2 Sprigatito.")
+        if(pathChoice4 == 2) {
+            alert(sprigatito);
+        }
+        else if(pathChoice4 == 1) {
+            alert(fuecoco);
+        }
+        else {
+            alert(invalid);
+        }
+    }
+    else if(pathChoice3 == "Right" || pathChoice3 == "right") {
+        alert(kantoText);
+        pathChoice5 = prompt("Which pokemon would you like to take? Enter 1 for Charmander or 2 for Squirtle.");
+        if(pathChoice5 == 2) {
+            alert(squirtle);
+        }
+        else if(pathChoice5 == 1) {
+            alert(charmander);
+        }
+        else {
+            alert(invalid);
+        }
+    }
+    else {
     alert(invalid);
+    }
+    alert(endText2);
 }
 
